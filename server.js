93925944129533307650adf1a7bc46ed4f89a75c
0219Andrew <hyunwoodoh0219@gmail.com>
2025-02-15 23:13:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app= express();
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use(express.json());
 
 app.get("/", (req, res) => {
     url = location.href;
-    res.sendFile(__dirname, "./index.html"); // fs를 안 써도 fs모듈 사용
+    res.sendFile(__dirname, "/public/index.html"); // fs를 안 써도 fs모듈 사용
 });
 
 app.get("/play", (req, res) => {
     console.log("href 통한 이동");
-    res.sendFile(__dirname, "./play.html"); // fs를 안 써도 fs모듈 사용
+    res.sendFile(__dirname, "/public/play.html"); // fs를 안 써도 fs모듈 사용
 });
 
 app.listen(3000, () => {
